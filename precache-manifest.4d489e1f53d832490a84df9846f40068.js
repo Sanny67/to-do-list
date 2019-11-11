07f76f1486516c9f7277c4977d3067321497f6c5
Sayantani <sayantanichatterjee67@gmail.com>
2019-11-11 13:18:35 +0530

    Updates

---
+++ precache-manifest.4d489e1f53d832490a84df9846f40068.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51ee5589451557ff25ef22480e2877c8",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "0c6451a510f7ecf4e95d",
+    "url": "/to-do-list/static/css/main.08d69ea2.chunk.css"
+  },
+  {
+    "revision": "a4766851937e1d529406",
+    "url": "/to-do-list/static/js/2.47a42948.chunk.js"
+  },
+  {
+    "revision": "0c6451a510f7ecf4e95d",
+    "url": "/to-do-list/static/js/main.1ad89938.chunk.js"
+  },
+  {
+    "revision": "ff05829fbf5acefe841b",
+    "url": "/to-do-list/static/js/runtime-main.fd3d8d31.js"
+  },
+  {
+    "revision": "f8ef8a2b97ec090bb3edb7e4d9b1f5fc",
+    "url": "/to-do-list/static/media/background1.f8ef8a2b.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/to-do-list/static/media/logo.25bf045c.svg"
+  }
+]);
